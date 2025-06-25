09d8ba14e4734ce7da08e05f88f95f87f8ab1019
cush media <cushmediagroup@gmail.com>
2025-06-25 01:22:00 +0000

    que crees panel settings no carga solo sale el de carga pero no pasa de

--- src/app/panel/settings/page.tsx
+++ src/app/panel/settings/page.tsx
@@ -58,7 +58,7 @@ export default function SettingsPage() {
                 form.reset(data);
             }
         })
-    }, [form]);
+    }, []);
 
 
     const onSubmit = (data: SiteSettingsSchema) => {
