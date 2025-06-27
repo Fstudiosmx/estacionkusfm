89c6eec9a8b05ec10239153ef54657b4f91bb0bd
Luis Martinez Sandoval <in3707989@gmail.com>
2025-06-27 01:05:38 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+{
+  "hosting": {
+    "rewrites": [
+      {
+        "source": "**",
+        "run": {
+          "serviceId": "estacionkusfm-prod",
+          "region": "us-central1"
+        }
+      }
+    ]
+  }
+}
